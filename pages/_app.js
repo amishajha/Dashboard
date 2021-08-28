c6b7e82d095d525f95755b33c469df6c3fde7c6b
Amisha Jha <jhaamisha096@gmail.com>
2021-08-28 23:43:13 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,13 @@
 import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
-import { extendTheme } from "@chakra-ui/react"
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      100: "#f7fafc",
-      // ...
-      900: "#1a202c",
-    },
-  },
-})
+
+
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Component {...pageProps}/>
     </ChakraProvider>
   ) 
